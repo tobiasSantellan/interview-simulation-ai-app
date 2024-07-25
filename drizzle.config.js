93dfce7fa97aa98f667d98f285d9279aea0da8d5
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-07-25 00:23:58 -0300

    Replace DrizzleKit database URL with environment variable

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   schema: "./utils/schema.js",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://neondb_owner:7F5LzeySIlEY@ep-old-wind-a5vns660.us-east-2.aws.neon.tech/neondb?sslmode=require",
+    url: NEXT_PUBLIC_DRIZZLE_DB_URL,
   },
 };
