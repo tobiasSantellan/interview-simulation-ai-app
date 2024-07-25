bc02582f13c4ec41278eaa1b825ce7007125377b
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-07-24 23:27:51 -0300

    Create Dashboard component with AddNewInterview and UserButton

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,11 +1,18 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
+import AddNewInterview from "./_components/AddNewInterview";
 
 function Dashboard() {
   return (
-    <div>
-      Dashboard
-      <UserButton />
+    <div className="p-4">
+      <h2 className="font-bold text-2xl">Dashboard</h2>
+      <h2 className="text-gray-500">
+        Prepare for success: Simulate your interview with AI
+      </h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 my-5">
+        <AddNewInterview />
+      </div>
     </div>
   );
 }
