e6f15b71b1e5280b6ad8990b52533866a38a8a79
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-07-26 17:38:11 -0300

    Implement interview page UI and fetch interview data from database. Includes components for displaying job details, webcam, and start button.

---
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -0,0 +1,99 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { db } from "@/utils/db";
+import { MockInterview } from "@/utils/schema";
+import { eq } from "drizzle-orm";
+import { Lightbulb, WebcamIcon } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import Webcam from "react-webcam";
+// The params object is essential for:
+// Access dynamic URL data: You can use params values ​​to query a database, search for data in an API, or simply display specific information on the page.
+// Create dynamic URLs: You can use params values ​​to generate links to other pages with different parameters.
+function Interview({ params }) {
+  const [interviewData, setInterviewData] = useState();
+  const [WebCamEnabled, setWebCamEnabled] = useState(false);
+  useEffect(() => {
+    console.log(params.interviewId);
+    GetInterviewDetails();
+  }, []);
+
+  // used to get interview details by mockId/interview ID
+  const GetInterviewDetails = async () => {
+    const result = await db
+      .select()
+      .from(MockInterview)
+      .where(eq(MockInterview.mockId, params.interviewId));
+
+    console.log(result);
+    setInterviewData(result[0]);
+  };
+  return (
+    <div className="my-10 ">
+      <h2 className="font-bold text-2xl  ">Let's Get Started</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        {" "}
+        <div className="flex flex-col my-5 gap-5 ">
+          <div className="flex flex-col gap-5 border p-5 rounded-lg">
+            <h2 className="text-lg">
+              <strong className="text-gray-800">Job Role/Job Position: </strong>
+              {interviewData?.jobPosition}
+            </h2>
+            <h2 className="text-lg">
+              <strong className="text-gray-800">
+                Job Decription/Tech Stack:{" "}
+              </strong>
+              {interviewData?.jobDesc}
+            </h2>
+            <h2 className="text-lg">
+              <strong
+                className="text-gray-800
+              "
+              >
+                Years of Experience:{" "}
+              </strong>
+              {interviewData?.jobExperience}
+            </h2>
+          </div>
+          <div className="p-5 border rounded-lg border-yellow-400 bg-yellow-100">
+            <h2 className="flex gap-2 items-center text-yellow-500">
+              {" "}
+              <Lightbulb /> <strong>Information</strong>
+            </h2>
+            <h2 className="mt-3 text-yellow-500">
+              {process.env.NEXT_PUBLIC_INFORMATION}
+            </h2>
+          </div>
+        </div>
+        <div>
+          {WebCamEnabled ? (
+            <Webcam
+              onUserMedia={() => setWebCamEnabled(true)}
+              onUserMediaError={() => setWebCamEnabled(false)}
+              mirrored={true}
+              style={{
+                height: 300,
+                width: 300,
+              }}
+            />
+          ) : (
+            <>
+              <WebcamIcon className="h-72 w-full my-7 p-20 bg-secondary rounded-lg border" />
+              <Button
+                variant="outline"
+                className="w-full"
+                onClick={() => setWebCamEnabled(true)}
+              >
+                Enable Web Cam and Microphone
+              </Button>
+            </>
+          )}
+        </div>
+      </div>
+      <div className="flex justify-end items-end">
+        <Button>Get Started</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Interview;
