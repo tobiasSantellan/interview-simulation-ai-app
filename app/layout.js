371b28912c2bd6f5bfd6df4490cd72ec535ef4d8
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-07-29 19:04:51 -0300

    Import Toaster component for displaying notifications

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "500", "600", "700", "800", "900"],
@@ -16,7 +17,10 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={poppins.className}>{children}</body>
+        <body className={poppins.className}>
+          <Toaster />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
