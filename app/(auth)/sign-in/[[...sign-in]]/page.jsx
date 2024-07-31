6739513b7b52941fa32b09be0779ed4ca4b439b9
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-07-31 17:58:49 -0300

    debuging

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,12 +1,12 @@
 import { SignIn } from "@clerk/nextjs";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Page() {
   return (
     <section className="bg-white">
       <div className="lg:grid lg:min-h-screen lg:grid-cols-12 ">
         <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
-          <Image
+          <img
             alt="ai-picture"
             src="ai-picture.jpg"
             className="absolute inset-0 h-full w-full object-center opacity-80"
