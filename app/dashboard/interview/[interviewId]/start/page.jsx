dd042981a13b55b3f1077ecacbe0b781180c7e6a
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-07-30 23:26:27 -0300

    Added navigation buttons (prev, next, finish), enabled question navigation, redirects to feedback screen on completion, and passed additional props to AnswerSection for enhanced functionality

--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -5,6 +5,8 @@ import { eq } from "drizzle-orm";
 import React, { useEffect, useState } from "react";
 import QuestionsSection from "./_components/QuestionsSection";
 import AnswerSection from "./_components/AnswerSection";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 function StartInterview({ params }) {
   const [interviewData, setInterviewData] = useState();
@@ -39,7 +41,32 @@ function StartInterview({ params }) {
         />
 
         {/* Video / Audio Recording */}
-        <AnswerSection />
+        <AnswerSection
+          mockInterviewQuestion={mockInterviewQuestion}
+          activeQuestionIndex={activeQuestionIndex}
+          interviewData={interviewData}
+        />
+      </div>
+      <div className="flex justify-end gap-3 mb-3">
+        {activeQuestionIndex > 0 && (
+          <Button
+            onClick={() => setActiveQuestionIndex(activeQuestionIndex - 1)}
+          >
+            Previous Question
+          </Button>
+        )}
+        {activeQuestionIndex != mockInterviewQuestion?.length - 1 && (
+          <Button
+            onClick={() => setActiveQuestionIndex(activeQuestionIndex + 1)}
+          >
+            Next Question
+          </Button>
+        )}
+        {activeQuestionIndex == mockInterviewQuestion?.length - 1 && (
+          <Link href={`/dashboard/interview/${interviewData?.mockId}/feedback`}>
+            <Button>End Interview</Button>
+          </Link>
+        )}
       </div>
     </div>
   );
