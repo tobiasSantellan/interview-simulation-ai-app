f50b37dba86278236b4d955b22c7da86aceff3a4
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-07-31 12:13:15 -0300

    remove ul

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -9,29 +9,6 @@ function Header() {
   return (
     <div className="flex p-4 justify-between items-center bg-secondary shadow-sm">
       <Image src={"/logo.svg"} width={160} height={100} alt="logo" />
-      <ul className=" hidden md:flex gap-6">
-        <li
-          className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${
-            path == "/dashboard" && "text-primary font-bold"
-          }`}
-        >
-          Dashboard
-        </li>
-        <li
-          className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${
-            path == "/dashboard/questions" && "text-primary font-bold"
-          }`}
-        >
-          Questions
-        </li>
-        <li
-          className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${
-            path == "/dashboard/how" && "text-primary font-bold"
-          }`}
-        >
-          How It Works?
-        </li>
-      </ul>
       <UserButton />
     </div>
   );
