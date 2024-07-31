8ac036be47d197f7ca4670228531a9f20480b79a
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-07-31 12:19:15 -0300

    added interviewList component

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,7 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
 import AddNewInterview from "./_components/AddNewInterview";
+import InterviewList from "./_components/InterviewList";
 
 function Dashboard() {
   return (
@@ -13,6 +14,8 @@ function Dashboard() {
       <div className="grid grid-cols-1 md:grid-cols-3 my-5">
         <AddNewInterview />
       </div>
+      {/* Previous Interview Question */}
+      <InterviewList />
     </div>
   );
 }
