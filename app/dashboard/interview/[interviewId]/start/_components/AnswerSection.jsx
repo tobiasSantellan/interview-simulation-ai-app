95989f8dcd9bf391a7b719d7b78b1781028c8879
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-07-30 23:23:10 -0300

    Implement feedback & rating integration with chatSession

--- app/dashboard/interview/[interviewId]/start/_components/AnswerSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/AnswerSection.jsx
@@ -1,14 +1,22 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { chatSession } from "@/utils/AIModal";
+import { db } from "@/utils/db";
+import { UserAnswer } from "@/utils/schema";
 import { useUser } from "@clerk/nextjs";
 import { Mic } from "lucide-react";
+import moment from "moment/moment";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import useSpeechToText from "react-hook-speech-to-text";
 import Webcam from "react-webcam";
 import { toast } from "sonner";
 
-function AnswerSection() {
+function AnswerSection({
+  mockInterviewQuestion,
+  activeQuestionIndex,
+  interviewData,
+}) {
   const [userAnswer, setUserAnswer] = useState("");
   const { user } = useUser();
   const [loading, setLoading] = useState(false);
@@ -17,6 +25,7 @@ function AnswerSection() {
     interimResult,
     isRecording,
     results,
+    setResults,
     startSpeechToText,
     stopSpeechToText,
   } = useSpeechToText({
@@ -30,16 +39,53 @@ function AnswerSection() {
     );
   }, [results]);
 
-  const SaveUserAnswer = () => {
+  useEffect(() => {
+    if (!isRecording && userAnswer?.length > 10) {
+      UpdateUserAnswer();
+    }
+  }, [userAnswer]);
+
+  const StartStopRecording = async () => {
     if (isRecording) {
       stopSpeechToText();
-      if (userAnswer?.length < 10) {
-        toast("Error while saving your answer. Please record again");
-        return;
-      }
     } else {
       startSpeechToText();
     }
+  };
+
+  const UpdateUserAnswer = async () => {
+    setLoading(true);
+    const feedbackPrompt = `Question: ${mockInterviewQuestion[activeQuestionIndex]?.question}, User Answer: ${userAnswer}. Depends on question and user answer for give interview question, please give us rating for answer and feedback as area of improvement if any in just 3 to 5 lines to improve it in JSON format with rating field and feedback field`;
+
+    const result = await chatSession.sendMessage(feedbackPrompt);
+    const MockJsonResp = result.response
+      .text()
+      .replace("```json", "")
+      .replace("```", "")
+      .trim();
+    // console.log(MockJsonResp);
+    const JsonFeedbackResponse = JSON.parse(MockJsonResp);
+    console.log(JsonFeedbackResponse);
+
+    const resp = await db.insert(UserAnswer).values({
+      mockIdRef: interviewData?.mockId,
+      question: mockInterviewQuestion[activeQuestionIndex]?.question,
+      correctAns: mockInterviewQuestion[activeQuestionIndex]?.answer,
+      userAns: userAnswer,
+      feedback: JsonFeedbackResponse?.feedback,
+      rating: JsonFeedbackResponse?.rating,
+      userEmail: user?.primaryEmailAddress?.emailAddress,
+      createdAt: moment().format("DD-MM-yyyy"),
+    });
+
+    if (resp) {
+      toast("User Answer recorded succesfully");
+      setUserAnswer("");
+      setResults([]);
+    }
+    setResults([]);
+
+    setLoading(false);
   };
   return (
     <div className="flex items-center justify-center flex-col">
@@ -59,7 +105,12 @@ function AnswerSection() {
           }}
         />
       </div>
-      <Button variant="ghost" className="my-10 " onClick={SaveUserAnswer}>
+      <Button
+        disabled={loading}
+        variant="ghost"
+        className="my-10 "
+        onClick={StartStopRecording}
+      >
         {isRecording ? (
           <h2 className="text-red-600 flex gap-2">
             <Mic />
@@ -71,8 +122,6 @@ function AnswerSection() {
           </h2>
         )}
       </Button>
-
-      <Button onClick={() => console.log(userAnswer)}>Show User Answer</Button>
     </div>
   );
 }
