db8e3b08eb796cb60920d44796bd936bc76ce20c
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-07-31 22:46:10 -0300

    update and debug

--- app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -14,17 +14,16 @@ function QuestionsSection({ mockInterviewQuestion, activeQuestionIndex }) {
     mockInterviewQuestion && (
       <div className="p-5 border rounded-lg my-10">
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-          {mockInterviewQuestion &&
-            mockInterviewQuestion.map((question, index) => (
-              <h2
-                key={index}
-                className={`p-2 border rounded-full text-xs md:text-sm text-center cursor-pointer ${
-                  activeQuestionIndex == index && "bg-primary text-white"
-                }`}
-              >
-                Question {index + 1}
-              </h2>
-            ))}
+          {mockInterviewQuestion.map((question, index) => (
+            <h2
+              key={index}
+              className={`p-2 border rounded-full text-xs md:text-sm text-center cursor-pointer ${
+                activeQuestionIndex === index && "bg-primary text-white"
+              }`}
+            >
+              Question {index + 1}
+            </h2>
+          ))}
         </div>
         <h2 className="my-5 text-md md:text-md">
           {mockInterviewQuestion[activeQuestionIndex]?.question}
